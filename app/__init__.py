c5176e2ff1008a5c911735d77ff97d17c6ee7db4
Matt Kafonek <kafonek@gmail.com>
2015-09-13 18:21:38 -0400

    Implement Homework1 form
    I've used Flask-WTforms to add a form to solve the UMUC Homework 1
    problem.  The changes are that in __init__.py, I added a secret key.  In
    views, I created a WTForm class and code in the '/hw1' endpoint to solve
    the problem.  In the hw1.html template, I added code to render the form
    and output.

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,8 @@ from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
+# Adding a SECRET_KEY is necessary to use forms with CSRF protection
+app.config['SECRET_KEY'] = 'umuc test project'
+
 
 from . import views
--- app/views.py
+++ app/views.py
@@ -1,13 +1,37 @@
 from . import app
-from flask import render_template
+from flask import render_template, request
+from flask_wtf import Form
+from wtforms import IntegerField, SelectField, SubmitField
+from wtforms.validators import Required, ValidationError
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/hw1')
+### TODO: move these into their own forms.py file
+class HW1(Form):
+	integer1 = IntegerField(label="First integer", validators=[Required()])
+	integer2 = IntegerField(label="Second integer", validators=[Required()])
+	operation = SelectField(label="Operation",  
+			                choices=[('+', 'addition'), ('-', 'subtraction'), ('*', 'multiplication'),
+			                		 ('/', 'division'), ('%', 'modulus'), ('&', 'bitwise and'),
+			                		 ('|', 'bitwise or')])
+	submit = SubmitField('Submit')
+
+
+@app.route('/hw1', methods=['GET', 'POST'])
 def hw1():
-	return render_template('hw1.html')
+	form = HW1()
+	output = None
+	if form.validate_on_submit():
+		int1 = form.data['integer1']
+		int2 = form.data['integer2']
+		operation = form.data['operation']
+		# normally eval isn't safe, but we're using sanitized input
+		result = eval('%s %s %s' % (int1, operation, int2)) 
+		output = "The result of %s %s %s is %s" % (int1, operation, int2, result)
+
+	return render_template('hw1.html', form=form, output=output)
 
 @app.route('/hw2')
 def h2():
