fd06ce259f3a27deaafe155e2b5c42f5b5224134
Matt Kafonek <kafonek@gmail.com>
2015-09-13 11:40:33 -0400

    Adding Bootstrap and two homework pages
    The first thing to look at in this commit is that we added
    Bootstrap(app) to app/__init__.py.  If you visit our webpage and look at
    the source code that the browser sees, you'll see it didn't do much
    except add three links to Bootstrap css / javascript CDN sites.
    
    Flask-Bootstrap also adds some convenience to our jinja2 templates and
    later we'll see how it works with WTForms.  Check out the docs linked in
    app/templates/base.html.

--- app/views.py
+++ app/views.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/hw1')
+def hw1():
+	return render_template('hw1.html')
+
+@app.route('/hw2')
+def h2():
+	return render_template('hw2.html')
